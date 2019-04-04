1e5ac2a95f2baba8bb76bb146e6455316c549046
alireza <easazade@gmail.com>
2019-04-05 00:52:05 +0430

    file templates - adding context as args

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Alireza',
+        'title': 'Blog Post',
+        'content': 'First post Content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Ahamd',
+        'title': 'Blog Post 2',
+        'content': 'Second post Content',
+        'date_posted': 'August 28, 2018'
+    },
+    {
+        'author': 'Saman',
+        'title': 'Blog Post 3',
+        'content': 'Third post Content',
+        'date_posted': 'August 29, 2018'
+    }
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html')
